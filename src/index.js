781bb420c28efb6fba01b38c9b9ce71be86e38ae
jacobsilver2 <jacobsilver2@mac.com>
2018-10-02 10:27:36 -0400

    added main css file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Client from 'shopify-buy';
-// import '../../shared/app.css';
+import './App.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
